d55e372f5505ed5fd1b616199bd8211b6b52af19
Michael Quigley <mikequigley@quigleypublishing.com>
2020-03-07 06:14:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  index !< 0 && !index > 8 && !position_taken(board, index)
+  index >= 0 && !index > 8 && !position_taken(board, index)
 end
 
 def position_taken?(board, index)
